2372fc04da547cbf58d258e4268a2e4711893280
Manohar.A <manohar.a@yellowblock.net>
2026-02-26 11:14:53 +0530

    Clean commit without service account key

---
+++ src/assets/svgicons/index.tsx
@@ -0,0 +1,36 @@
+
+import MenuIcon from './menuicon.svg';
+import SearchIcon from './searchicon.svg';
+import CartIcon from './carticon.svg';
+import CopyIcon from './copy.svg';
+
+import NavAddressIcon from './nav_address.svg';
+import NavCouponsIocn from './nav_coupons.svg';
+import NavHomeIcon from './nav_home_white.svg';
+import NavLogoutIcon from './nav_logout.svg';
+import NavNetworkIcon from './nav_network.svg';
+import NavPackagesIcon from './nav_packages.svg';
+import NavUserIcon from './nav_profile.svg';
+import NavReferralIcon from './nav_referral.svg';
+import NavSettingsIcon from './nav_settings.svg';
+import NavSupportIcon from './nav_support.svg';
+import NavPayeeIcon from './nav-payees.svg';
+
+
+export {
+    MenuIcon,
+    SearchIcon,
+    CartIcon,
+    CopyIcon,
+    NavAddressIcon,
+    NavCouponsIocn,
+    NavHomeIcon,
+    NavLogoutIcon,
+    NavNetworkIcon,
+    NavPackagesIcon,
+    NavUserIcon,
+    NavReferralIcon,
+    NavSettingsIcon,
+    NavSupportIcon,
+    NavPayeeIcon,
+}
